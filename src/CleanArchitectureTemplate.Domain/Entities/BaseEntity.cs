4e769afc0306a23743ad7b2df82d599783177b5a
WaleedEmad173 <waleed.emad173@gmail.com>
2026-09-23 12:06:28 +0300

    Enhance BaseEntity with audit and soft delete fields
    Updated BaseEntity to remove default CreatedAtUtc initialization and add CreatedBy, UpdatedAtUtc, UpdatedBy, IsDeleted, DeletedAtUtc, and DeletedBy for improved audit and soft deletion support.

--- src/CleanArchitectureTemplate.Domain/Entities/BaseEntity.cs
+++ src/CleanArchitectureTemplate.Domain/Entities/BaseEntity.cs
@@ -3,6 +3,14 @@ namespace CleanArchitectureTemplate.Domain.Entities;
 public abstract class BaseEntity
 {
     public int Id { get; set; }
-    public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+
+    public DateTime CreatedAtUtc { get; set; }
+    public int? CreatedBy { get; set; }
+
     public DateTime? UpdatedAtUtc { get; set; }
+    public int? UpdatedBy { get; set; }
+
+    public bool IsDeleted { get; set; }
+    public DateTime? DeletedAtUtc { get; set; }
+    public int? DeletedBy { get; set; }
 }
